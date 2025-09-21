15f63da6fcb544e06da4d8db3de3ac54978601db
Bullet2004 <buithecuong2004@gmail.com>
2025-09-21 15:45:17 +0700

    System-Shop: Created OrderPage

--- system-shop/src/App.jsx
+++ system-shop/src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import Sidebar from "./components/Sidebar";
+import TopNav from "./components/TopNav";
+import Tabs from "./components/Tabs";
+import FilterBar from "./components/FilterBar";
+import EmptyState from "./components/EmptyState";
+import Orders from "./components/Orders";
+import CreateOrderPage from "./pages/CreateOrderPage";
+
+const App = () => {
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex min-h-screen bg-gray-50">
+      <Sidebar active="Quản lý đơn hàng" />
+      
+        <CreateOrderPage/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
