c639aac94f8912e28cc8ce68271f9ab2765d157b
trhieu27 <trunghieu271004@gmail.com>
2025-09-21 21:49:55 +0700

    Mobile: Implement UI login

---
+++ mobile-shipping/express_mail/lib/resources/strings.dart
@@ -0,0 +1,18 @@
+class AppStrings {
+  static const login = "Đăng nhập";
+  static const username = "Tài khoản";
+  static const password = "Mật khẩu";
+  static const empty_fields_error = "Vui lòng nhập đầy đủ thông tin";
+  static const fast_delivery ="Giao hàng nhanh";
+  static const your_delivery_partner = "Đối tác giao hàng của bạn";
+  static const welcome = "Chào mừng!";
+  static const login_to_start_delivery = "Đăng nhập để bắt đầu giao hàng";
+  static const or = "Hoặc";
+  static const continue_with_email = "Tiếp tục với Email";
+  static const continue_with_phone_number = "Tiếp tục với số điện thoại";
+  static const continue_with_username = "Tiếp tục với tài khoản";
+  static const phone_number = "Số điện thoại";
+  static const email = "Email";
+  static const register = "Đăng ký tài khoản";
+  static const forgot_password = "Quên mật khẩu?";
+}
---
+++ mobile-shipping/express_mail/lib/ui/home/HomeActivity.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class HomeActivity extends StatelessWidget {
+  const HomeActivity({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
