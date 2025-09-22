2e2b91f4c69bc70795f75a4ff5054970209968fd
Tai <bintolo1b@gmail.com>
2025-09-22 22:08:13 +0700

    API: Init user and post office models

--- express-mail-django-api/shared/messages.py
+++ express-mail-django-api/shared/messages.py
@@ -1,18 +1,11 @@
-from django.utils.translation import gettext_lazy as _
 from django.conf import settings
 
 # Use this way instead of using translations for custom messages
 _ERROR_MESSAGES = {
     "en": {
         "common": {
-            "invalid_token": _("Invalid token."),
-            "invalid_basic_auth": _("Invalid basic authentication credentials."),
-        }
-    },
-    "ja": {
-        "common": {
-            "invalid_token": _("トークンは無効です。"),
-            "invalid_basic_auth": _("基本認証資格情報は無効です。"),
+            "invalid_token": "Invalid token.",
+            "invalid_basic_auth": "Invalid basic authentication credentials.",
         }
     },
 }
