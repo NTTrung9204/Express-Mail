c5bf247f3fe2a1536a39820ad0dcd4efd25dd3dc
Tai <bintolo1b@gmail.com>
2025-10-09 11:49:15 +0700

    Django-API: Enforce view permission of DjangoModelPermissions

--- express-mail-django-api/shared/permissions.py
+++ express-mail-django-api/shared/permissions.py
@@ -1,4 +1,20 @@
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import BasePermission, DjangoModelPermissions
+
+
+class FullDjangoModelPermissions(DjangoModelPermissions):
+    """
+    Extended version of DjangoModelPermissions that also enforces `view_` permissions.
+    """
+
+    perms_map = {
+        "GET": ["%(app_label)s.view_%(model_name)s"],
+        "OPTIONS": [],
+        "HEAD": [],
+        "POST": ["%(app_label)s.add_%(model_name)s"],
+        "PUT": ["%(app_label)s.change_%(model_name)s"],
+        "PATCH": ["%(app_label)s.change_%(model_name)s"],
+        "DELETE": ["%(app_label)s.delete_%(model_name)s"],
+    }
 
 
 class GenericMultiPermission(BasePermission):
