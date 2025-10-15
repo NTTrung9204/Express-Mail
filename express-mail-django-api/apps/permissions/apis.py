f9921a1774235a84d427cb09075fd7c0a0cff941
Tai <bintolo1b@gmail.com>
2025-10-15 15:25:12 +0700

    Django-API: Implement get all permissions of a group API

--- express-mail-django-api/apps/permissions/apis.py
+++ express-mail-django-api/apps/permissions/apis.py
@@ -1,8 +1,12 @@
 from django.contrib.auth.models import Permission, Group
 from drf_spectacular.utils import extend_schema
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from apps.permissions.serializers import PermissionSerializer, GroupSerializer
+from services.permissions.permission_services import PermissionService
 from shared.permissions import FullDjangoModelPermissions
 
 
@@ -12,7 +16,7 @@ class PermissionViewSet(ReadOnlyModelViewSet):
     API endpoint for Permission model.
     """
 
-    queryset = Permission.objects.select_related("content_type").all()
+    queryset = Permission.objects.all()
     serializer_class = PermissionSerializer
     permission_classes = [FullDjangoModelPermissions]
     pagination_class = None
@@ -24,7 +28,19 @@ class GroupViewSet(ReadOnlyModelViewSet):
     API endpoint for Group model.
     """
 
-    queryset = Group.objects.prefetch_related("permissions")
+    queryset = Group.objects.all()
     serializer_class = GroupSerializer
     permission_classes = [FullDjangoModelPermissions]
     pagination_class = None
+
+    @action(detail=True, methods=["get"], url_path="permissions")
+    def permissions(self, request, pk=None):
+        """
+        Get all permissions of a group.
+        """
+
+        group = self.get_object()
+        permissions = PermissionService.get_permissions_of_group(group)
+        return Response(
+            PermissionSerializer(permissions, many=True).data, status=status.HTTP_200_OK
+        )
