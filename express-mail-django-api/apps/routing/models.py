691625e4853d612cd905dcaf9100611b44bc0184
Tai <bintolo1b@gmail.com>
2025-11-08 09:58:34 +0700

    Django-API: Add VRP API calling permission

--- express-mail-django-api/apps/routing/models.py
+++ express-mail-django-api/apps/routing/models.py
@@ -1,1 +1,18 @@
-# Create your models here.
+from django.db import models
+
+
+class Routing(models.Model):
+    """
+    Dummy model for defining custom permissions for the Routing app.
+    """
+
+    class Meta:
+        """
+        Meta class for Routing dummy model.
+        """
+
+        managed = False
+        default_permissions = ()
+        permissions = [
+            ("call_vrp", "Can call VRP API"),
+        ]
