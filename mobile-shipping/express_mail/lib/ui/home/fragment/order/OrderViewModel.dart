9114ccfd140ecde95cccff41791e443fbc1cf0b1
trhieu27 <trunghieu271004@gmail.com>
2025-11-10 22:52:53 +0700

    Mobile: Implement update order fragment

--- mobile-shipping/express_mail/lib/ui/home/fragment/order/OrderViewModel.dart
+++ mobile-shipping/express_mail/lib/ui/home/fragment/order/OrderViewModel.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:express_mail/data/enum/ShippingStatus.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:express_mail/data/model/DetailOrder.dart';
@@ -12,6 +13,10 @@ class OrderViewModel extends ChangeNotifier {
   bool isLoadingShipping = false;
   bool isLoadingReturning = false;
   bool isLoadingAll = false;
+
+  bool _isCompletingOrder = false;
+
+  bool get isCompletingOrder => _isCompletingOrder;
 
   // --- Error message ---
   String? errorMessage;
@@ -34,12 +39,12 @@ class OrderViewModel extends ChangeNotifier {
 
   // --- Fetch Orders General ---
   Future<void> fetchOrders(
-      LoginResponse loginResponse, {
-        required String? status,
-        required int? page,
-        required ValueSetter<List<DetailOrder>> onSuccess,
-        required ValueSetter<bool> setLoading,
-      }) async {
+    LoginResponse loginResponse, {
+    required String? status,
+    required int page,
+    required ValueSetter<List<DetailOrder>> onSuccess,
+    required ValueSetter<bool> setLoading,
+  }) async {
     setLoading(true);
     errorMessage = null;
     notifyListeners();
@@ -47,12 +52,14 @@ class OrderViewModel extends ChangeNotifier {
     String url = '${Constants.shipperOrderUrl}${loginResponse.user.id}?';
     if (status != null) url += 'status=$status&';
     url += 'page=$page&limit=${Constants.limit}';
-
     try {
-      final response = await http.get(Uri.parse(url), headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ${loginResponse.access}',
-      });
+      final response = await http.get(
+        Uri.parse(url),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ${loginResponse.access}',
+        },
+      );
 
       if (response.statusCode != 200) {
         final error = jsonDecode(response.body);
@@ -62,30 +69,44 @@ class OrderViewModel extends ChangeNotifier {
         return;
       }
 
-      final dataList = jsonDecode(response.body)['data']?['data'] ?? [];
-      final orders = dataList
-          .map<DetailOrder>(
-              (item) => DetailOrder.fromJson(Map<String, dynamic>.from(item)))
-          .toList();
-
+      final meta = jsonDecode(response.body)['data']?['meta'];
       switch (status) {
         case "PICKUP_REQUESTED":
-          pickupTotalPages = jsonDecode(response.body)['data']?['meta']['totalPages'];
-          pickupRequestCount = jsonDecode(response.body)['data']?['meta']['total'];
+          pickupTotalPages = meta['totalPages'] ?? 1;
+          pickupRequestCount = meta['total'] ?? 0;
           break;
         case "SHIPPING":
-          shippingTotalPages = jsonDecode(response.body)['data']?['meta']['totalPages'];
-          shippingCount = jsonDecode(response.body)['data']?['meta']['total'];
+          shippingTotalPages = meta['totalPages'] ?? 1;
+          shippingCount = meta['total'] ?? 0;
           break;
         case "RETURNING":
-          returningTotalPages = jsonDecode(response.body)['data']?['meta']['totalPages'];
-          returningCount = jsonDecode(response.body)['data']?['meta']['total'];
+          returningTotalPages = meta['totalPages'] ?? 1;
+          returningCount = meta['total'] ?? 0;
           break;
         default:
-          allTotalPages = jsonDecode(response.body)['data']?['meta']['totalPages'];
-          allCount = jsonDecode(response.body)['data']?['meta']['total'];
+          allTotalPages = meta['totalPages'] ?? 1;
+          allCount = meta['total'] ?? 0;
           break;
       }
+
+      final dataList = jsonDecode(response.body)['data']?['data'] ?? [];
+
+      List<DetailOrder> orders = dataList
+          .map<DetailOrder>(
+            (item) => DetailOrder.fromJson(Map<String, dynamic>.from(item)),
+          )
+          .toList();
+      orders = orders
+          .where((order) => order.status != ShippingStatus.FINISHED)
+          .toList();
+
+      if (status == null) {
+        allCount = orders.length;
+        allTotalPages = (allCount / Constants.limit).ceil();
+      }
+
+      print("SSSS ${allCount} ${allTotalPages}");
+
       onSuccess(orders);
     } catch (e) {
       errorMessage = '${AppStrings.connection_error}: $e';
@@ -95,8 +116,11 @@ class OrderViewModel extends ChangeNotifier {
     }
   }
 
-  // --- Fetch Pickup Request Orders ---
-  Future<void> fetchPickupRequestOrders(LoginResponse loginResponse, {int page = 1}) async {
+  // --- Fetch Orders by status ---
+  Future<void> fetchPickupRequestOrders(
+    LoginResponse loginResponse, {
+    int page = 1,
+  }) async {
     await fetchOrders(
       loginResponse,
       status: "PICKUP_REQUESTED",
@@ -106,8 +130,10 @@ class OrderViewModel extends ChangeNotifier {
     );
   }
 
-  // --- Fetch Shipping Orders ---
-  Future<void> fetchShippingOrders(LoginResponse loginResponse, {int page = 1}) async {
+  Future<void> fetchShippingOrders(
+    LoginResponse loginResponse, {
+    int page = 1,
+  }) async {
     await fetchOrders(
       loginResponse,
       status: "SHIPPING",
@@ -117,8 +143,10 @@ class OrderViewModel extends ChangeNotifier {
     );
   }
 
-  // --- Fetch Returning Orders ---
-  Future<void> fetchReturningOrders(LoginResponse loginResponse, {int page = 1}) async {
+  Future<void> fetchReturningOrders(
+    LoginResponse loginResponse, {
+    int page = 1,
+  }) async {
     await fetchOrders(
       loginResponse,
       status: "RETURNING",
@@ -128,8 +156,10 @@ class OrderViewModel extends ChangeNotifier {
     );
   }
 
-  // --- Fetch All Orders ---
-  Future<void> fetchAllOrders(LoginResponse loginResponse, {int page = 1}) async {
+  Future<void> fetchAllOrders(
+    LoginResponse loginResponse, {
+    int page = 1,
+  }) async {
     await fetchOrders(
       loginResponse,
       status: null,
@@ -137,5 +167,55 @@ class OrderViewModel extends ChangeNotifier {
       onSuccess: (orders) => allOrders = orders,
       setLoading: (loading) => isLoadingAll = loading,
     );
+  }
+
+  // --- Complete order by orderId ---
+  Future<bool> completeOrderById(
+    LoginResponse loginResponse,
+    int orderId,
+    int pageAll,
+    int pagePickup,
+    int pageShipping,
+    int pageReturn,
+  ) async {
+    _isCompletingOrder = true;
+    notifyListeners();
+
+    final url = "${Constants.baseUrlTrung}/shipping";
+
+    try {
+      final response = await http.post(
+        Uri.parse(url),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ${loginResponse.access}',
+          'accept': 'application/json',
+        },
+        body: jsonEncode({
+          "orderId": orderId,
+          "status": "FINISHED",
+          "shipperId": loginResponse.user.id.toString(),
+        }),
+      );
+
+      if (response.statusCode == 201) {
+        await Future.wait([
+          fetchAllOrders(loginResponse, page: pageAll),
+          fetchPickupRequestOrders(loginResponse, page: pagePickup),
+          fetchShippingOrders(loginResponse, page: pageShipping),
+          fetchReturningOrders(loginResponse, page: pageReturn),
+        ]);
+
+        notifyListeners();
+        return true;
+      } else {
+        return false;
+      }
+    } catch (_) {
+      return false;
+    } finally {
+      _isCompletingOrder = false;
+      notifyListeners();
+    }
   }
 }
