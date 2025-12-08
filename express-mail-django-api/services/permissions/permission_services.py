55590fcffe11d9e6eafa774b16d3031369702b9d
Tai <bintolo1b@gmail.com>
2025-12-08 22:59:38 +0700

    Django-API: Remove encrypt permissions in jwt

--- express-mail-django-api/services/permissions/permission_services.py
+++ express-mail-django-api/services/permissions/permission_services.py
@@ -1,6 +1,4 @@
-from utils.encrypt import Encrypt
 from utils.gzip import GzipString
-from django.conf import settings
 
 
 class PermissionService:
@@ -27,12 +25,11 @@ class PermissionService:
         return group.permissions.all()
 
     @staticmethod
-    def encrypt_permissions(permissions):
+    def gzip_permissions(permissions):
         """
-        Encrypt set of permissions into a string.
+        Gzip set of permissions into a string.
         """
 
         str_permissions = ",".join(sorted(permissions)) if permissions else ""
         gzip_base64_encode = GzipString.encode(str_permissions)
-        encrypt_password = settings.ENCRYPT_PASSWORD
-        return Encrypt.encrypt_text(gzip_base64_encode, encrypt_password)
+        return gzip_base64_encode
